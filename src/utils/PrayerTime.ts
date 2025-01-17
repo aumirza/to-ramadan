3530ac16ac7ea6df6d032cdb618575292f819afb
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-01-17 22:05:30 +0530

    feat: implement caching for prayer times based on location and date

--- src/utils/PrayerTime.ts
+++ src/utils/PrayerTime.ts
@@ -1,6 +1,18 @@
 import { CalculationMethod, Coordinates, PrayerTimes } from "adhan";
+import { DateTime } from "luxon";
+
+//memoise date and location
+const prayerTimesCache = new Map<string, PrayerTimes>();
 
 export function getPrayerTime(location: GeolocationCoordinates, date: Date) {
+  const key = `${location.latitude},${location.longitude},${DateTime.fromJSDate(
+    date
+  ).set({ hour: 0, minute: 0, second: 0, millisecond: 0 })}`;
+
+  if (prayerTimesCache.has(key)) {
+    return prayerTimesCache.get(key) as PrayerTimes;
+  }
+
   const prayerTimes = new PrayerTimes(
     new Coordinates(location.latitude, location.longitude),
     date,
@@ -10,6 +22,8 @@ export function getPrayerTime(location: GeolocationCoordinates, date: Date) {
       nightPortions: CalculationMethod.Karachi().nightPortions,
     }
   );
+
+  prayerTimesCache.set(key, prayerTimes);
 
   return prayerTimes;
 }
