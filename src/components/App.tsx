96316c44c2da7f381d4a0f14b0c18c2687fe597b
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-01-20 12:05:46 +0530

    feat: integrate location fetching and add loading prompt in App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,34 @@
+import { useLocation } from "@/hooks/useLocation";
 import DuaCards from "./DuaCards";
 import Header from "./Header";
 import Prayers from "./Prayers";
 import Rakats from "./Rakats";
 import { TimeTabs } from "./TimeTabs";
 import { Toaster } from "./ui/sonner";
+import Footer from "./Footer";
 
 function App() {
+  const { location, isFetchingLocation } = useLocation();
   return (
-    <div className="relative flex flex-col items-center justify-center w-11/12 h-full min-h-screen gap-10 mx-auto">
+    <div className="relative flex flex-col items-center h-full min-h-screen gap-10">
+      {!isFetchingLocation && !location ? (
+        <div className="fixed top-0 left-0 z-30 flex items-center justify-center w-screen h-screen bg-black/80">
+          <h2 className="text-2xl text-white">
+            Please allow location access to work.
+          </h2>
+        </div>
+      ) : (
+        ""
+      )}
       <Header />
-      <div className="flex flex-col items-center justify-center flex-grow gap-10">
+      <main className="flex flex-col items-center justify-center flex-grow w-11/12 gap-10">
         <DuaCards />
         <TimeTabs />
         <Prayers />
         <Rakats />
-      </div>
+      </main>
       <Toaster />
+      <Footer />
     </div>
   );
 }
