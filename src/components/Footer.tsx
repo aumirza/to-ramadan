b035c2c4c73d87e8186f8a618a08a69351a2bbe4
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-01-20 12:05:39 +0530

    feat: add Footer component with attribution and styling

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <div className="relative w-full py-5 bg-gray-200">
+      <p className="text-center">
+        Made with ❤️ by{" "}
+        <a href="https://ahmadullah.in" className="underline">
+          Aumirza
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
