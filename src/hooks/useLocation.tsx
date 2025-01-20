14858e70155f6dc66a19e3779257e6efb2a780d1
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-01-20 12:05:33 +0530

    fix: add isFetchingLocation state to manage location fetching status

--- src/hooks/useLocation.tsx
+++ src/hooks/useLocation.tsx
@@ -14,6 +14,7 @@ import { toast } from "sonner";
 type LocationContextType = {
   location: GeolocationCoordinates | undefined;
   setLocation: (location: GeolocationCoordinates) => void;
+  isFetchingLocation: boolean;
   refreshLocation: () => void;
 };
 
@@ -34,22 +35,30 @@ export const useLocation = () => {
 // LocationProvider component
 export function LocationProvider({ children }: { children: ReactNode }) {
   const [location, setLocation] = useState<GeolocationCoordinates>();
+  const [fetching, setFetching] = useState(false);
 
   const fetchLocation = async () => {
+    if (fetching) return;
+    setFetching(true);
     const storedLocation = localStorage.getItem("location");
     if (storedLocation) {
       setLocation(JSON.parse(storedLocation));
+      setFetching(false);
       return;
     }
+    setFetching(false);
     try {
+      setFetching(true);
       toast.loading("Fetching location...", { duration: 1000 });
       const coords = await getLocation();
-      toast.dismiss();
       setLocation(coords);
       localStorage.setItem("location", JSON.stringify(coords));
     } catch (error) {
       toast.error("Failed to fetch location");
       console.error("Error fetching location:", error);
+    } finally {
+      setFetching(false);
+      toast.dismiss();
     }
   };
 
@@ -67,9 +76,10 @@ export function LocationProvider({ children }: { children: ReactNode }) {
     () => ({
       location,
       setLocation,
+      isFetchingLocation: fetching,
       refreshLocation, // Expose fetchLocation to consumers
     }),
-    [location, refreshLocation]
+    [location, refreshLocation, fetching]
   );
 
   return (
